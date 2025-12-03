ce45679f7185645efcf69878b0a85a616002df11
NicoleWu20120422 <83037228+NicoleWu20120422@users.noreply.github.com>
2025-12-02 18:12:39 -0700

    Create Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to Supplier Information Management</h1>
+      <p>Use this app to manage your suppliers and their documents.</p>
+    </div>
+  );
+}
+export default Home;
