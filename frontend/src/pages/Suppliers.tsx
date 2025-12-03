0fe2964701277935c022625a458d7852c667ef79
NicoleWu20120422 <83037228+NicoleWu20120422@users.noreply.github.com>
2025-12-02 18:13:17 -0700

    Create Suppliers.tsx

---
+++ frontend/src/pages/Suppliers.tsx
@@ -0,0 +1,11 @@
+import SupplierList from '../components/SupplierList';
+
+function Suppliers() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Suppliers</h2>
+      <SupplierList />
+    </div>
+  );
+}
+export default Suppliers;
