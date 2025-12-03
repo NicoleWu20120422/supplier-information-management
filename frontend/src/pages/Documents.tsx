264309e979e58da7ee38887764c08016baf53652
NicoleWu20120422 <83037228+NicoleWu20120422@users.noreply.github.com>
2025-12-02 18:13:49 -0700

    Create Documents.tsx

---
+++ frontend/src/pages/Documents.tsx
@@ -0,0 +1,11 @@
+import DocumentList from '../components/DocumentList';
+
+function Documents() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Documents</h2>
+      <DocumentList />
+    </div>
+  );
+}
+export default Documents;
